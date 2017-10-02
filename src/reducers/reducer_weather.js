97bd0f89b4bd779ce137fe1dfae1892492f72368
Albert Carrete <acarrete@Alberts-MacBook-Pro.local>
2017-10-02 16:05:29 -0700

    wired in reducer, does NOT manipulate state

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,21 @@
+import { FETCH_WEATHER } from '../actions/index'
+
 // remember reducers are just functions
-export default function(state = null, action){
+// initial state is an array because we are going to have a list of cities
+export default function(state = [], action){
+
+	switch(action.type){
+		case FETCH_WEATHER:
+			// NEVER MANIPULATE STATE
+			// We use concat because it doesn't mutate state 
+			// we are returning a new version of our state
+			// return state.concat([action.payload.data]);
+			// Or we can use the ES6 way, which makes a new array
+			// then puts action.payload.data inside it and then
+			// takes that state array and inserts it in 
+			// adds the record at the top of the array.
+			return [ action.payload.data, ...state ] // [city, city ,city ] NOT [city, [city, city]]
+	}
+
 	return state;
 }
