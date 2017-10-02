13c771e997c1724932ead4bae75723defaac0768
Albert Carrete <acarrete@Alberts-MacBook-Pro.local>
2017-10-02 13:43:46 -0700

    created a new Action Creator: FetchWeather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = 'eb904305acf2c866905b9bd267153cba';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+
+	const url=`${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return{
+		type: FETCH_WEATHER,
+		payload: request
+	}
+}
