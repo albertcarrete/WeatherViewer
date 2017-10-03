4c7328f5892b9d464b2895719c985c75839b5a87
Albert Carrete <acarrete@Alberts-MacBook-Pro.local>
2017-10-03 10:48:47 -0700

    made charts reusable and labeled units

--- src/components/chart.js
+++ src/components/chart.js
@@ -6,15 +6,23 @@
 // we don't need to make any use of component state, we just recieve data and
 // that's it so we can get away with making a function component here
 
+import _ from 'lodash'
 import React from 'react'
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+function average(data){
+    return _.round(_.sum(data)/data.length);
+}
+
 
 export default (props) => {
     return(
         <div>
             <Sparklines height={120} width={180} data={props.data}>
                 <SparklinesLine color={props.color} />
-            </Sparklines>            
+                <SparklinesReferenceLine type="avg" />
+            </Sparklines>
+            <div>{average(props.data)} {props.units}</div>        
         </div>
     )
 }
